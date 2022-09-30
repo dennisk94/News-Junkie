959d9535f27fa301557dc08549d92d6c58749668
Dennis Kim <dennis.kim@hotmail.com>
2022-09-29 21:08:25 -0700

    add footer styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,59 @@
 <template>
-  <footer class="footer"></footer>
+  <footer class="footer">
+    <div class="categories">
+      <div class="category-cell left">
+        <a href="/">Technology</a>
+        <a href="/">Business</a>
+        <a href="/">Science</a>
+        <a href="/">Health</a>
+      </div>
+      <div class="category-cell right">
+        <a href="/">Sports</a>
+        <a href="/">General</a>
+        <a href="/">Science</a>
+      </div>
+    </div>
+    <p class="copy">&copy; {{ getYear() }} || Dennis Kim</p>
+  </footer>
 </template>
 
 <script>
+import { getYear } from "../utilities/getDates";
+
 export default {
   setup() {
-    return {};
+    return { getYear };
   },
 };
 </script>
 
 <style>
+.footer {
+  background-color: var(--nav-bgc);
+  padding: 1rem 0;
+}
+.categories {
+  display: flex;
+  justify-content: space-around;
+}
+.category-cell a {
+  text-decoration: none;
+  color: var(--white);
+  font-size: 0.9rem;
+  margin: 0.5rem 0;
+}
+.category-cell a:hover {
+  text-decoration: underline;
+}
+.left,
+.right {
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  align-items: flex-start;
+}
+.copy {
+  margin: 1rem 0 0 0;
+  color: white;
+}
 </style>
