91b15d43b1942de1845f6ff075e6a4173722e1f7
Dennis Kim <dennis.kim@hotmail.com>
2022-09-29 20:31:51 -0700

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <h1 class="app-name">News Junkie</h1>
+    </div>
+    <div v-if="!isActive">
+      <BIconList class="hamburger" @click="toggleMenu" />
+    </div>
+    <div v-if="isActive">
+      <BIconFilterRight class="hamburger" @click="toggleMenu" />
+    </div>
+    <Nav :isActive="isActive" />
+  </header>
+</template>
+
+<script>
+import Nav from "./Nav.vue";
+import { BIconList, BIconFilterRight } from "bootstrap-icons-vue";
+
+// Vue imports
+import { ref } from "vue";
+
+export default {
+  components: {
+    Nav,
+    BIconList,
+    BIconFilterRight,
+  },
+  setup() {
+    const isActive = ref(false);
+
+    const toggleMenu = () => {
+      isActive.value = !isActive.value;
+      console.log(isActive);
+    };
+    return { toggleMenu, isActive };
+  },
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 0.7rem;
+}
+.app-name {
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+}
+.app-name:hover {
+  color: var(--primary);
+}
+.hamburger {
+  cursor: pointer;
+  font-size: 2rem;
+  transition: all 0.2s ease-in-out;
+}
+.hamburger:hover {
+  color: var(--primary);
+}
+</style>
