0f1abd51a3a0b2b309868f3f82cae70ac1436e5a
Dennis Kim <dennis.kim@hotmail.com>
2022-10-03 14:41:55 -0700

    add search page view

---
+++ src/views/SearchView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search-page">
+    <div class="search-input">
+      <input class="input" type="text" placeholder="Search..." />
+      <span class="search-icon"><BIconSearch /></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BIconSearch } from "bootstrap-icons-vue";
+
+export default {
+  components: { BIconSearch },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.search-input {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 2rem 0.5rem;
+}
+.input {
+  border: none;
+  border-radius: 10px;
+  width: 70%;
+  font-size: 1.3rem;
+  padding: 1rem 1rem;
+  background-color: #cacaca;
+}
+.input:focus {
+  background-color: #ebebeb;
+}
+.search-icon {
+  position: absolute;
+  right: 0;
+  cursor: pointer;
+  background-color: green;
+  padding: 1rem;
+  border: none;
+  border-radius: 10px;
+}
+.search-icon svg {
+  color: white;
+  font-size: 1.3rem;
+}
+</style>
