d4a1898a064f3b13ba878be48e0bf23f344e1fdb
Dennis Kim <dennis.kim@hotmail.com>
2022-10-03 14:43:11 -0700

    add search view to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: SearchView
   },
 ]
 
