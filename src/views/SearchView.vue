704adabdd048de54151e18bc7bc88523e7df471e
Dennis Kim <dennis.kim@hotmail.com>
2022-10-03 15:07:22 -0700

    add misc

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -20,17 +20,17 @@ export default {
 
 <style>
 .search-input {
-  position: relative;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   margin: 2rem 0.5rem;
 }
 .input {
   border: none;
-  border-radius: 10px;
+  border-radius: 10px 0 0 10px;
   width: 70%;
   font-size: 1.3rem;
+  margin: 0;
   padding: 1rem 1rem;
   background-color: var(--input-bgc);
 }
@@ -38,16 +38,34 @@ export default {
   background-color: var(--input-focus);
 }
 .search-icon {
-  position: absolute;
-  right: 0;
   cursor: pointer;
-  background-color: green;
-  padding: 1rem;
+  background-color: var(--primary);
+  padding: 1.05rem 1rem;
   border: none;
-  border-radius: 10px;
+  border-radius: 0 10px 10px 0;
+  transition: all 0.2s ease-in-out;
+}
+.search-icon:hover {
+  background-color: var(--secondary);
 }
 .search-icon svg {
-  color: white;
-  font-size: 1.3rem;
+  color: var(--white);
+  font-size: 1.1rem;
+}
+@media (min-width: 37.5em) {
+  .search-input {
+    margin: 5rem 0.5rem 2rem 0.5rem;
+  }
+}
+@media (min-width: 50em) {
+  .search-input {
+    margin: 3rem auto 2rem auto;
+    max-width: 1000px;
+  }
+}
+@media (min-width: 62.5em) {
+  .search-icon {
+    padding: 1.075rem 1rem;
+  }
 }
 </style>
