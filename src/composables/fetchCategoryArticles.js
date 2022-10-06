2e48427715e30043752674aa187e6305589f44c3
Dennis Kim <dennis.kim@hotmail.com>
2022-10-05 20:59:42 -0700

    add fetch functionality for category articles

---
+++ src/composables/fetchCategoryArticles.js
@@ -0,0 +1,31 @@
+import { APIKEY } from "@/globals/globals";
+import { ref } from "vue";
+
+const fetchCategoryArticles = ( category ) => {
+    const articleData = ref([])
+    const error = ref(null)
+
+    const fetchArticles = async (category) => {
+        try {
+            const res = await fetch(`https://newsapi.org/v2/top-headlines?category=${ category }&language=en`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + APIKEY
+                }
+            });
+            const newsData = await res.json()
+            console.log(newsData);
+            const randomNumber1 = Math.floor(Math.random() * 17);
+            const randomNumber2 = randomNumber1 + 3;
+            // console.log(randomNumber1, randomNumber2);
+
+            articleData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+            console.log(newsData.articles.slice( randomNumber1, randomNumber2 ));
+        } catch (err) {
+            error.value = err.message
+        }
+    }
+    return { articleData, error, fetchArticles }
+}
+
+export default fetchCategoryArticles
