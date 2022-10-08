8b6ff48f77b2476e4f32d1d0b9e174e52ff87544
Dennis Kim <dennis.kim@hotmail.com>
2022-10-08 00:23:50 -0700

    add dynamic content for single category view

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,142 +1,112 @@
 <template>
   <div class="category-page">
     <div class="articles-wrapper">
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
-      <a href="/article/123" class="article">
-        <img :src="placeholder" alt="title" />
-        <h3 class="article-title">
-          article title goes here. Thank you very much.
-        </h3>
-        <p class="article-excerpt">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          dolorem aliquid cum aut exercitationem unde.
-        </p>
-        <div class="article-attribution-container">
-          <p class="article-source">BBC</p>
-          <p class="article-published">05/01/2021</p>
-        </div>
-      </a>
+      <div
+        class="featured-category-article"
+        v-for="featuredArticle in featuredcategoryArticle"
+        :key="featuredArticle.title"
+      >
+        <a
+          :href="`/article/${featuredArticle.title}`"
+          class="featured-article"
+          v-if="featuredArticle.urlToImage"
+        >
+          <img :src="featuredArticle.urlToImage" :alt="featuredArticle.title" />
+          <h3 class="featured-article-title">
+            {{ featuredArticle.title }}
+          </h3>
+          <div class="featured-article-attribution-container">
+            <p class="featured-article-source">
+              {{ featuredArticle.source.name }}
+            </p>
+          </div>
+        </a>
+      </div>
+      <div
+        class="article-wrapper"
+        v-for="article in categoryArticles"
+        :key="article.title"
+      >
+        <a
+          :href="`/article/${article.title}`"
+          class="article"
+          v-if="article.urlToImage"
+        >
+          <img :src="article.urlToImage" :alt="article.title" />
+          <h3 class="article-title">
+            {{ article.title }}
+          </h3>
+          <div v-if="article.content">
+            <p class="article-excerpt">
+              {{ excerptGenerator(article.content) }}
+            </p>
+          </div>
+          <div v-else>
+            <p class="article-excerpt">Read more...</p>
+          </div>
+          <div class="article-attribution-container">
+            <p class="article-source">{{ article.source.name }}</p>
+            <p class="article-published">
+              {{ article.publishedAt.substring(0, 10) }}
+            </p>
+          </div>
+        </a>
+        <a :href="`/article/${article.title}`" class="article" v-else>
+          <img
+            :src="noImg"
+            alt="placeholder"
+            :class="{ 'article-thumbnail': true, noImg: !article.urlToImage }"
+          />
+          <h3 class="article-title">
+            {{ article.title }}
+          </h3>
+          <div v-if="article.content">
+            <p class="article-excerpt">
+              {{ excerptGenerator(article.content) }}
+            </p>
+          </div>
+          <div v-else>
+            <p class="article-excerpt">Read more...</p>
+          </div>
+          <div class="article-attribution-container">
+            <p class="article-source">{{ article.source.name }}</p>
+            <p class="article-published">
+              {{ article.publishedAt.substring(0, 10) }}
+            </p>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import placeholder from "../assets/img/placeholder.jpg";
+import { useRouter } from "vue-router";
+import fetchCategoryArticles from "../composables/fetchCategoryArticles";
+import noImg from "../assets/img/noImg.svg";
+import { excerptGenerator } from "../composables/excerptGenerator";
 
 export default {
   setup() {
-    return { placeholder };
+    // Route instance
+    const router = useRouter();
+    // The current category the user has clicked on
+    const category = router.currentRoute.value.params.slug;
+    // The current type the user has clicked on. eg. country or category
+    const type = router.currentRoute.value.name.toLowerCase();
+    // * Fetch articles
+    const { categoryArticles, featuredcategoryArticle, fetchArticles } =
+      fetchCategoryArticles();
+    // * Fetch all data for single category page
+    fetchArticles(category, type, "single");
+    return {
+      placeholder,
+      categoryArticles,
+      featuredcategoryArticle,
+      noImg,
+      excerptGenerator,
+    };
   },
 };
 </script>
