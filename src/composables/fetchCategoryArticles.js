050085066b4cb70b641ae5261680ac1a0d6f258a
Dennis Kim <dennis.kim@hotmail.com>
2022-10-08 00:22:13 -0700

    refactor fetch category articles code to return appropriate data for single category page

--- src/composables/fetchCategoryArticles.js
+++ src/composables/fetchCategoryArticles.js
@@ -8,10 +8,12 @@ const fetchCategoryArticles = () => {
     const countryData = ref([])
     const healthData = ref([])
     const generalData = ref([])
+    const categoryArticles = ref([])
+    const featuredcategoryArticle = ref([])
 
     const error = ref(null)
 
-    const fetchArticles = async (category, type) => {
+    const fetchArticles = async (category, type, path) => {
         const query = type === 'country' ? `country=${ category }` : `category=${ category }`;
 
         try {
@@ -22,36 +24,44 @@ const fetchCategoryArticles = () => {
                 }
             });
             const newsData = await res.json()
-            const randomNumber1 = Math.floor(Math.random() * 17);
-            const randomNumber2 = randomNumber1 + 3;
-            switch ( category ) {
-                case 'technology':
-                    technologyData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                case 'business':
-                    businessData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                case 'sports':
-                    sportsData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                case 'us':
-                    countryData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                case 'health':
-                    healthData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                case 'general':
-                    generalData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
-                    break;
-                default:
-                    break;
+            // When fetching data from a single category page
+            if ( path === 'single' ) {
+                const randomNumber1 = Math.floor(Math.random() * 10) + 1;
+                const randomNumber2 = randomNumber1 + 10;
+                featuredcategoryArticle.value = newsData.articles.slice(0, 1)
+                categoryArticles.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+            // When fetching data from the home page
+            } else if ( path === 'home' ) {
+                const randomNumber1 = Math.floor(Math.random() * 17);
+                const randomNumber2 = randomNumber1 + 3;
+                switch ( category ) {
+                    case 'technology':
+                        technologyData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    case 'business':
+                        businessData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    case 'sports':
+                        sportsData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    case 'us':
+                        countryData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    case 'health':
+                        healthData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    case 'general':
+                        generalData.value = newsData.articles.slice( randomNumber1, randomNumber2 )
+                        break;
+                    default:
+                        break;
+                }
             }
-            console.log(healthData.value);
         } catch (err) {
             error.value = err.message
         }
     }
-    return { technologyData, businessData, sportsData, countryData, healthData, generalData, error, fetchArticles }
+    return { technologyData, businessData, sportsData, countryData, healthData, generalData, categoryArticles, featuredcategoryArticle, error, fetchArticles }
 }
 
 export default fetchCategoryArticles
