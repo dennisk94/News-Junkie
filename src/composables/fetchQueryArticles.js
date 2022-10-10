2b60f5a9c33b018372b486708bc427ff0a6b74cf
Dennis Kim <dennis.kim@hotmail.com>
2022-10-09 19:20:45 -0700

    add functionality for search page query

---
+++ src/composables/fetchQueryArticles.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+import { APIKEY } from "@/globals/globals";
+
+const fetchQueryArticles = () => {
+    const queriedArticles = ref(null)
+    const error = ref(null)
+
+    const fetchQueriedArticles = async ( q ) => {
+        try {
+            const res = await fetch(`https://newsapi.org/v2/everything?q=${ q }`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + APIKEY
+                }
+            });
+            const newsData = await res.json()
+            queriedArticles.value = newsData.articles.slice( 0, 20 );
+        } catch (err) {
+            error.value = err.message
+        }
+    }
+    return { queriedArticles, error, fetchQueriedArticles }
+}
+
+export default fetchQueryArticles
